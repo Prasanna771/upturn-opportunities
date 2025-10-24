28cea50f0abc51896c672da686adfb2d0a434624
prasanna reddy <142732594+Prasanna771@users.noreply.github.com>
2025-10-24 21:30:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Montserrat, Poppins } from 'next/font/google';
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-//import { ThemeProvider } from "@/components/ThemeProvider";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const montserrat = Montserrat({
   subsets: ['latin'],
